9ad558feba07bc33bf25b0bd642419f0b5deb98e
Gonzalo Navarro <gnavarrofisio@gmail.com>
2022-01-11 20:47:19 +0100

    lab updated

--- module-1/advanced-mysql/solution.sql
+++ module-1/advanced-mysql/solution.sql
@@ -50,5 +50,10 @@ FROM dbo.#step2 step2
 GROUP BY step2.[Author ID]
 ORDER BY [Total Profit] DESC
 
+--CHALLENGE 3
+SELECT step3.[Author ID], step3.[Total Profit]
+INTO dbo.most_profiting_authors
+FROM dbo.#final_result step3
+
 SELECT *
-FROM dbo.#final_result
+FROM dbo.most_profiting_authors
